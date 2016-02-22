4e2cfed76b880bee95ccea0448cb3b22eab7aa46
Ting Zhou <tzhouhc@gmail.com>
2016-02-22 01:32:25 -0500

    slightly improved style; batch-mark-done; more comments; improved sorting

--- todo.rb
+++ todo.rb
@@ -9,30 +9,44 @@ parser = TimeParse.new(TimeLex.new)
 
 opts = Trollop.options do
   opt :add, 'Add an item to the to-do list.', default: nil, type: :string
-  opt :done, 'Mark an item on the to-do list as done.', default: nil, type: :int
+  opt :done, 'Mark an item on the to-do list as done.', default: nil, type: :ints
   opt :by, 'Designate a deadline for this todo.', default: nil, type: :string
 end
 
 todo_storage = ENV['HOME'] + '/.todo'
 File.write(todo_storage, YAML.dump([])) unless File.file?(todo_storage)
 
+def date_colorize(date)
+  # urgency color-code the date
+  date_diff = (date - Date.today).to_i
+  if date_diff < 2
+    date_diff.to_s.red
+  elsif date_diff > 5
+    date_diff.to_s.green
+  else
+    date_diff.to_s.yellow
+  end
+end
+
 def print_task(n, line)
+  # print the task by given info
   task, date = line
   if date
-    puts " #{n.to_s.yellow}\t| #{task.blue}: due in #{(date - Date.today).to_i.to_s.red} days"
-  else
+    date_s = date_colorize(date) # if there is a deadline
+    puts " #{n.to_s.yellow}\t| #{task.blue}: due in #{date_s} days"
+  else # if there is not 
     puts " #{n.to_s.yellow}\t| #{task.blue}"
   end
 end
 
 if !opts.add && !opts.done
-  # display mode'
+  # display mode
   File.open(todo_storage, 'r') do |file|
     content = file.read
     tasklist = YAML.load(content)
     n = 0
     tasklist.each do |line|
-      print_task(n, line)
+      print_task(n, line) # print one line at a time
       n += 1
     end
   end
@@ -42,15 +56,21 @@ elsif opts.add
   tasklist = [[opts.add, date]]
   File.open(todo_storage, 'r') do |file|
     content = file.read
-    tasklist += YAML.load(content)
+    tasklist += YAML.load(content) # read in the rest
   end
-  tasklist.sort_by! { |e| e[1].to_s }
+  tasklist.sort_by! do |e| # sort: by date, and put nil last
+    if e[1]
+      e[1].to_s # this would be a date, like '2016-02-01'
+    else
+      '9999-99-99' # this would probably be after that
+    end
+  end
   n = 0
   tasklist.each do |line|
-    print_task(n, line)
+    print_task(n, line) # also print out post-change
     n += 1
   end
-  data = YAML.dump(tasklist)
+  data = YAML.dump(tasklist) # then store the data
   File.write(todo_storage, data)
 else
   # mark done mode
@@ -58,13 +78,13 @@ else
   File.open(todo_storage, 'r') do |file|
     content = file.read
     tasklist = YAML.load(content)
-    tasklist.delete_at(opts.done)
+    tasklist.delete_if.with_index { |_, index| opts.done.include?(index) } # remove each after retrieve
     n = 0
-    tasklist.each do |line|
+    tasklist.each do |line| # also print out post-change
       print_task(n, line)
       n += 1
     end
-    data = YAML.dump(tasklist)
+    data = YAML.dump(tasklist) # then store the data
     File.write(todo_storage, data)
   end
 end
